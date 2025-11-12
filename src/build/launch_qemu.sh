776e4ecebcb63f21d30370a63568f8f88fd0f452
Tommy <ttomislav0323@gmail.com>
2025-11-12 22:02:27 +0100

    Add dm-verity-autoboot implementation and related updates

---
+++ src/build/launch_qemu.sh
@@ -0,0 +1,17 @@
+-append "console=ttyS0 loglevel=7 root=/dev/dm-0 rootfstype=ext4 rootwait dm_verity_autoboot.autoboot_device=/dev/vda dm_verity_autoboot.mode=verify_and_map"
+#!/bin/bash
+set -euo pipefail
+
+KERNEL='../../linux/arch/x86_64/boot/bzImage'
+ROOTFS='Binaries/rootfs.img'
+
+echo "====================================="
+echo " Launching QEMU with dm-verity setup "
+echo "====================================="
+
+exec qemu-system-x86_64 \
+  -kernel "$KERNEL" \
+  -drive if=none,file="$ROOTFS",format=raw,id=hd0 \
+  -device virtio-blk-pci,drive=hd0 \
+  -append "console=ttyS0 loglevel=7 root=/dev/dm-0 rootfstype=ext4 rootwait dm_verity_autoboot.autoboot_device=/dev/vda" \
+  -m 1024M -nographic
